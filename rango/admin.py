f7bc17146d3b7281523522125f7f3d9e651fe9e5
Philipp Hanemann <philipp.hanemann@posteo.de>
2017-11-26 12:11:37 +0100

    9.5 additional user attributes

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 
 class AdminPage(admin.ModelAdmin):
@@ -12,4 +12,5 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, AdminPage)
+admin.site.register(UserProfile)
 # Register your models here.
